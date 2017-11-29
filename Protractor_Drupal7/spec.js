18398ae5ee975f3ca74913d8b01502fbe20092e8
SDasman <shijit.dasgupta@gmail.com>
2017-11-29 16:26:52 -0500

    Update spec.js

--- Protractor_Drupal7/spec.js
+++ Protractor_Drupal7/spec.js
@@ -1,18 +1,20 @@
 browser.waitForAngularEnabled(false);
 describe('Enter User Credentials', function() {
- it('This will insert an userName', function() {
+ it('This will insert a test account that works', function() {
  browser.get('http://drupal-7-56.dd:8083/');
 
- element(by.xpath('//*[@id="edit-name"]')).sendKeys('Sdasgupta');
+ element(by.xpath('//*[@id="edit-name"]')).sendKeys('testuser');
   var username = element(by.xpath('//*[@id="edit-name"]'));
  
- element(by.xpath('//*[@id="edit-pass"]')).sendKeys('chicken');
+ element(by.xpath('//*[@id="edit-pass"]')).sendKeys('testpassword');
      var password = element(by.xpath('//*[@id="edit-pass"]'));
 
-     expect(username = 'Sdasgupta');
-     expect(password = 'chicken');
+     expect(username = 'testuser');
+     expect(password = 'testpassword');
      var login_button = element(by.xpath('//*[@id="edit-submit"]'));     
+     browser.sleep(1000);
      login_button.click();
+   browser.sleep(1000);
    browser.pause();
      });
 });
