7a5975f8292b8e0264d624653bb46543f99ee73f
SDasman <shijit.dasgupta@gmail.com>
2017-11-28 23:52:59 -0500

    Update spec.js

--- Protractor_Google/spec.js
+++ Protractor_Google/spec.js
@@ -1,10 +1,15 @@
 browser.waitForAngularEnabled(false);
-describe('Enter Search Term', function() {
- it('This will insert into the text field in google.com', function() {
- browser.get('www.google.com');
- element(by.xpath('//*[@id="q"]')).sendKeys('What is Protractor?');
-  var query = element(by.xpath('//*[@id="q"]'));
-expect(query = 'What is Protractor?');
-browser.pause();
-  });
+describe('Enter User Credentials', function() {
+ it('This will insert an userName', function() {
+ browser.get('http://google.com');
+
+ element(by.xpath('//*[@id="lst-ib"]')).sendKeys('what is protractor');
+  var query = element(by.xpath('//*[@id="lst-ib"]'));
+ 
+     expect(query = 'what is protractor');
+
+  var button = element(by.xpath('//*[@id="tsf"]/div[2]/div[3]/center/input[1]'));
+  button.click();
+   browser.pause();
+     });
 });
